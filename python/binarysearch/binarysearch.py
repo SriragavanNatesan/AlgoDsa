e979a365fd42f57c4a9eca6837b1f69a1d9922b5
N, SRIRAGAVAN <sn@ciena.com>
2021-12-29 21:57:47 +0530

    binary search update

--- python/binarysearch/binarysearch.py
+++ python/binarysearch/binarysearch.py
@@ -4,6 +4,10 @@ def binarysearch(arr, start, end, k):
     # print("start = {}, end={}".format(start, end))
     if start > end:
         return -1
+    '''
+    TO avoid mid overflow if len(arr) > 10^9
+    mid = start +  int((end -start) / 2)    
+    '''
     mid  = int((start + end) / 2)
     if arr[mid] == k:
         return mid
