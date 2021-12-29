7a860d79400ab89260cf9b886d3bf4345198d1d1
N, SRIRAGAVAN <sn@ciena.com>
2021-12-29 23:18:28 +0530

    last occurence

---
+++ python/binarysearch/last_occurence.py
@@ -0,0 +1,28 @@
+'''
+last_occurence
+'''
+
+arr = [2, 4, 10, 10, 10, 18, 20, 20, 27]
+print(arr)
+
+def binarysearch(arr, start, end, k):
+    if start > end:
+        return -1
+    # if start == end:
+    #     if arr[start] == k:
+    #         return start
+    #     else:
+    #         return -1
+    mid = int((start + end) / 2)
+    if arr[mid] == k:
+        if binarysearch(arr, mid+1  , end, k) == -1:
+            return mid
+        else:
+            return binarysearch(arr, mid, end, k)
+    elif arr[mid] > k :
+        return binarysearch(arr, start, mid-1, k)
+    else:
+        return binarysearch(arr, mid+1, end, k)
+
+
+print(binarysearch(arr, 0, len(arr)-1, 18))
