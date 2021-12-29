caa037148959fa97a2fa4a52ae2324d802f09e72
N, SRIRAGAVAN <sn@ciena.com>
2021-12-29 21:55:07 +0530

    update

--- python/binarysearch/binarysearch.py
+++ python/binarysearch/binarysearch.py
@@ -1,11 +1,10 @@
 arr = [1, 3, 5, 6, 9, 11, 33]
 
 def binarysearch(arr, start, end, k):
-    print("start = {}, end={}".format(start, end))
+    # print("start = {}, end={}".format(start, end))
     if start > end:
         return -1
     mid  = int((start + end) / 2)
-    print(mid)
     if arr[mid] == k:
         return mid
     elif arr[mid] > k:
