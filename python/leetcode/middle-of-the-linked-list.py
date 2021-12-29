ae2f20bbf43b80da999753805fa1e5f6285b6100
N, SRIRAGAVAN <sn@ciena.com>
2021-12-29 21:21:58 +0530

    yesterday leetcode challange

---
+++ python/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+'''
+
+https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+'''
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
